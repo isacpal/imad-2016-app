e84f350c8a01a5e03ce8fc010e61f1cf5d750c65
isacpal <isaacpaul.p@gmail.com>
2016-10-06 14:59:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var element=getElementById('main-text');
+element.innerHTML='New Value';  
+var img=document.getElementById('img');
+img.onClick= function(){
+  img.style.marginleft='100px';  
+};
+
