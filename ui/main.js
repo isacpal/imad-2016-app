eaad12bc3c798add9562a6d253b6f5df43c769bf
isacpal <isaacpaul.p@gmail.com>
2016-10-06 16:25:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var element = getElementById('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML='New Value';  
 var img=document.getElementById('img');
 img.onClick= function(){
